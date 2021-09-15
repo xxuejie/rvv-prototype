39fa12b111ff7c6df1284fcb9d1305abb00a712c
Xuejie Xiao <xxuejie@gmail.com>
2021-09-15 07:19:17 +0000

    Proc macro based skeleton

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,7 +1,9 @@
+#![feature(asm)]
+
 extern crate rvv;
 use rvv::rvv_vector;
 
-use std::ops::{Add, Sub};
+use std::ops::{Add, Sub, Mul};
 
 pub struct U256(u8);
 
@@ -21,11 +23,27 @@ impl Sub for U256 {
     }
 }
 
+impl Mul for U256 {
+    type Output = Self;
+
+    fn mul(self, _other: Self) -> Self {
+        unimplemented!()
+    }
+}
+
 #[rvv_vector]
 fn foo(a: U256, b: U256, c: U256) -> U256 {
-    a + b - c
+    a + b * c
 }
 
 fn main() {
-    println!("Hello, world!");
+    let a = U256(1);
+    let b = U256(2);
+    let c = U256(3);
+
+    let d = foo(a, b, c);
+
+    if d.0 > 0 {
+        println!("Hello, world!");
+    }
 }
--- rvv/src/lib.rs
+++ rvv/src/lib.rs
@@ -1,12 +1,43 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, ItemFn};
+use quote::quote;
+use syn::{parse_macro_input, Expr, FnArg, ItemFn, fold::{Fold}};
+
+struct RvvTransformer(u8);
+
+impl Fold for RvvTransformer {
+    fn fold_fn_arg(&mut self, i: FnArg) -> FnArg {
+        self.0 += 1;
+        println!("Folding fn arg: {:?}", i);
+        i
+    }
+
+    fn fold_expr(&mut self, e: Expr) -> Expr {
+        println!("Folding expr: {:#?}", e);
+        e
+    }
+
+    // fn fold_block(&mut self, _block: Block) -> Block {
+        // let i = self.0;
+        // let block: Block = parse_quote! {
+            // {
+    	          // unsafe {
+    		            // asm!(".byte 0xba, {}, 0x0, 0x0, 0x0", const #i);
+    	          // }
+    	          // unimplemented!()
+    	      // }
+        // };
+        // block
+    // }
+}
 
 #[proc_macro_attribute]
 pub fn rvv_vector(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let item2 = item.clone();
     let input = parse_macro_input!(item as ItemFn);
-    println!("Parsed item: {:#?}", input);
 
-    item2
+    let mut transformer = RvvTransformer(10);
+
+    let output = transformer.fold_item_fn(input);
+
+    TokenStream::from(quote!(#output))
 }
