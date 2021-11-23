51627045d076be795e31438bdda55559ce160ad9
xjd <xjd@cryptape.com>
2021-11-23 10:51:07 +0800

    use rvv-simulator-runtime in alt-bn128-example
    move mont-example and alt-bn128-example to their own workspaces

--- bn128-example/src/main.rs
+++ bn128-example/src/main.rs
@@ -6,7 +6,8 @@
 #![feature(panic_info_message)]
 
 extern crate rvv;
-use ckb_std::{debug, default_alloc};
+use alloc::format;
+use ckb_std::{default_alloc, syscalls::debug};
 ckb_std::entry!(program_entry);
 default_alloc!();
 
@@ -15,8 +16,9 @@ fn main() {
     // debug!("test_alt_bn128_add     pass");
     // alt_bn128::ethereum::ut::test_alt_bn128_mul();
     // debug!("test_alt_bn128_mul     pass");
-    alt_bn128::ethereum::ut::test_alt_bn128_pairing();
-    debug!("test_alt_bn128_pairing pass");
+    debug(format!("start test_alt_bn128_pairing ..."));
+    alt_bn128_example::ethereum::ut::test_alt_bn128_pairing();
+    debug(format!("test_alt_bn128_pairing pass"));
 }
 
 pub fn program_entry() -> i8 {
