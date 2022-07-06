784ec51b6d003f8c0428e6a90902fbb0537b6ba7
Xuejie Xiao <xxuejie@gmail.com>
2022-07-06 14:20:57 +0800

    Some initial Gfp6 work, SoA change pending

--- bn128-example/src/rvv_crypto/gfp6.rs
+++ bn128-example/src/rvv_crypto/gfp6.rs
@@ -1,18 +1,31 @@
-use super::gfp2;
+use super::{constants::*, gfp2::Gfp2, gfp::Gfp};
 
 #[derive(Clone, Debug, Default, PartialEq)]
-pub struct Gfp6(pub [gfp2::Gfp2; 3]);
+pub struct Gfp6(pub [Gfp2; 3]);
+
+fn constant_to_gfp2(a: &[[u64; 4]; 2]) -> Gfp2 {
+    Gfp2([Gfp(a[0]), Gfp(a[1])])
+}
 
 impl Gfp6 {
-    pub fn x(&self) -> &gfp2::Gfp2 {
+    pub fn x(&self) -> &Gfp2 {
         &self.0[0]
     }
 
-    pub fn y(&self) -> &gfp2::Gfp2 {
+    pub fn y(&self) -> &Gfp2 {
         &self.0[1]
     }
 
-    pub fn z(&self) -> &gfp2::Gfp2 {
+    pub fn z(&self) -> &Gfp2 {
         &self.0[2]
+    }
+
+    pub fn frobenius(&mut self) {
+        self.0[0].conjugate();
+        self.0[1].conjugate();
+        self.0[2].conjugate();
+
+        self.0[0].mul_ref(&constant_to_gfp2(&XI_TO_2P_MINUS2_OVER3));
+        self.0[1].mul_ref(&constant_to_gfp2(&XI_TO_P_MINUS1_OVER3));
     }
 }
