096aa3dba386f6fdf9156afed231ec84ce31c998
Xuejie Xiao <xxuejie@gmail.com>
2022-07-06 15:30:21 +0800

    Let's take one more step to build a Gfp12 function
    We can then see how to work backwards on SoA design

--- bn128-example/src/rvv_crypto/gfp12.rs
+++ bn128-example/src/rvv_crypto/gfp12.rs
@@ -1,14 +1,32 @@
-use super::gfp6;
+use super::gfp6::Gfp6;
 
 #[derive(Clone, Debug, Default, PartialEq)]
-pub struct Gfp12(pub [gfp6::Gfp6; 2]);
+pub struct Gfp12(pub [Gfp6; 2]);
 
 impl Gfp12 {
-    pub fn x(&self) -> &gfp6::Gfp6 {
+    pub fn x(&self) -> &Gfp6 {
         &self.0[0]
     }
 
-    pub fn y(&self) -> &gfp6::Gfp6 {
+    pub fn y(&self) -> &Gfp6 {
         &self.0[1]
+    }
+
+    pub fn mul(&mut self, b: &Gfp12) {
+        let mut tx = self.x().clone();
+        tx.mul_ref(b.y());
+        let mut t = b.x().clone();
+        t.mul_ref(self.y());
+        tx.add_ref(&t);
+
+        let mut ty = self.y().clone();
+        ty.mul_ref(b.y());
+        t.set(self.x());
+        t.mul_ref(b.x());
+        t.mul_tau();
+
+        self.0[0].set(&tx);
+        self.0[1].set(&ty);
+        self.0[1].add_ref(&t);
     }
 }
--- bn128-example/src/rvv_crypto/gfp6.rs
+++ bn128-example/src/rvv_crypto/gfp6.rs
@@ -20,6 +20,12 @@ impl Gfp6 {
         &self.0[2]
     }
 
+    pub fn set(&mut self, other: &Gfp6) {
+        self.0[0].set(&other.0[0]);
+        self.0[1].set(&other.0[1]);
+        self.0[2].set(&other.0[2]);
+    }
+
     pub fn frobenius(&mut self) {
         self.0[0].conjugate();
         self.0[1].conjugate();
@@ -29,29 +35,63 @@ impl Gfp6 {
         self.0[1].mul_ref(&constant_to_gfp2(&XI_TO_P_MINUS1_OVER3));
     }
 
-    pub fn mul(&mut self, b: &Gfp6) {
-        let mut v0 = self.z().clone(); v0.mul_ref(b.z());
-        let mut v1 = self.y().clone(); v1.mul_ref(b.y());
-        let mut v2 = self.x().clone(); v2.mul_ref(b.x());
+    pub fn mul_ref(&mut self, b: &Gfp6) {
+        let mut v0 = self.z().clone();
+        v0.mul_ref(b.z());
+        let mut v1 = self.y().clone();
+        v1.mul_ref(b.y());
+        let mut v2 = self.x().clone();
+        v2.mul_ref(b.x());
 
-        let mut t0 = self.x().clone(); t0.add_ref(self.y());
-        let mut t1 = b.x().clone(); t1.add_ref(b.y());
-        let mut tz = t0.clone(); tz.mul_ref(&t1);
-        tz.sub_ref(&v1); tz.sub_ref(&v2); tz.mul_xi(); tz.add_ref(&v0);
+        let mut t0 = self.x().clone();
+        t0.add_ref(self.y());
+        let mut t1 = b.x().clone();
+        t1.add_ref(b.y());
+        let mut tz = t0.clone();
+        tz.mul_ref(&t1);
+        tz.sub_ref(&v1);
+        tz.sub_ref(&v2);
+        tz.mul_xi();
+        tz.add_ref(&v0);
 
-        t0.set(self.y()); t0.add_ref(self.z());
-        t1.set(b.y()); t1.add_ref(b.z());
-        let mut ty = t0.clone(); ty.mul_ref(&t1);
-        t0.set(&v2); t0.mul_xi();
-        ty.sub_ref(&v0); ty.sub_ref(&v1); ty.add_ref(&t0);
+        t0.set(self.y());
+        t0.add_ref(self.z());
+        t1.set(b.y());
+        t1.add_ref(b.z());
+        let mut ty = t0.clone();
+        ty.mul_ref(&t1);
+        t0.set(&v2);
+        t0.mul_xi();
+        ty.sub_ref(&v0);
+        ty.sub_ref(&v1);
+        ty.add_ref(&t0);
 
-        t0.set(self.x()); t0.add_ref(self.z());
-        t1.set(b.x()); t1.add_ref(b.z());
-        let mut tx = t0.clone(); tx.mul_ref(&t1);
-        tx.sub_ref(&v0); tx.add_ref(&v1); tx.sub_ref(&v2);
+        t0.set(self.x());
+        t0.add_ref(self.z());
+        t1.set(b.x());
+        t1.add_ref(b.z());
+        let mut tx = t0.clone();
+        tx.mul_ref(&t1);
+        tx.sub_ref(&v0);
+        tx.add_ref(&v1);
+        tx.sub_ref(&v2);
 
         self.0[0].set(&tx);
         self.0[1].set(&ty);
+        self.0[2].set(&tz);
+    }
+
+    pub fn add_ref(&mut self, b: &Gfp6) {
+        self.0[0].add_ref(&b.0[0]);
+        self.0[1].add_ref(&b.0[1]);
+        self.0[2].add_ref(&b.0[2]);
+    }
+
+    pub fn mul_tau(&mut self) {
+        let mut tz = self.x().clone();
+        tz.mul_xi();
+        self.0[0].set(&self.y().clone());
+        self.0[1].set(&self.z().clone());
         self.0[2].set(&tz);
     }
 }
